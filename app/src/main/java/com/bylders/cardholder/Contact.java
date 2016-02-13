1d80ba16fdaacac56dea638d326722917fc3dc42
Sambhav Satija <sambhav13085@iiitd.ac.in>
2016-02-13 20:31:37 +0530

    added login

---
+++ app/src/main/java/com/bylders/cardholder/Contact.java
@@ -0,0 +1,26 @@
+package com.bylders.cardholder;
+
+import android.graphics.Bitmap;
+
+import java.io.Serializable;
+
+/**
+ * Created by darkryder on 13/2/16.
+ */
+public class Contact implements Serializable {
+    public String name;
+    public String pk;
+    public String contact_image_url;
+    public String mobile;
+    public String email;
+    public String website;
+
+    public Contact(String name, String pk, String contact_image_url, String mobile, String email, String website) {
+        this.name = name;
+        this.pk = pk;
+        this.contact_image_url = contact_image_url;
+        this.mobile = mobile;
+        this.email = email;
+        this.website = website;
+    }
+}
